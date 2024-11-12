51f3fb39596015bde12b51f4853009294fa2cce4
Juan Bailke <juan_bailke@hotmail.com>
2024-11-12 20:15:49 -0300

    Game DTO, repository, service and controller

---
+++ src/main/java/com/juanbailke/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.juanbailke.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.juanbailke.dslist.dto.GameMinDTO;
+import com.juanbailke.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value="/games")
+public class GameController {
+	
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll(){
+		List<GameMinDTO> result = gameService.findAll();
+		return result;
+	}
+
+}
---
+++ src/main/java/com/juanbailke/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.juanbailke.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.juanbailke.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
