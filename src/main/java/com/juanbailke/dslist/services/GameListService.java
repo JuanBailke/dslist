fc76207f40c4c23a9ff23030827883bbfb2f9950
Juan Bailke <juan_bailke@hotmail.com>
2024-11-15 00:33:11 -0300

    Move games on list

--- src/main/java/com/juanbailke/dslist/services/GameListService.java
+++ src/main/java/com/juanbailke/dslist/services/GameListService.java
@@ -7,19 +7,38 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.juanbailke.dslist.dto.GameListDTO;
-import com.juanbailke.dslist.dto.GameMinDTO;
+import com.juanbailke.dslist.projections.GameMinProjection;
 import com.juanbailke.dslist.repositories.GameListRepository;
+import com.juanbailke.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 	
 	@Autowired
 	private GameListRepository gameListRepository;
-
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		var result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
 	}
 
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		var list = gameRepository.searchByList(listId);
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for(int i=min; i<=max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+	}
+	
 }
